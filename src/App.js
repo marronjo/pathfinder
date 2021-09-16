960552bf2c610d813e053d9ddb8e466e6e033936
Joe Marron <68810618+marronjo@users.noreply.github.com>
2021-09-16 19:01:58 +0100

    dijkstra

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pathfinder from './pathfinder/Pathfinder';
+
+function App() {
+  return (
+    <div className="App">
+      <Pathfinder></Pathfinder>
+    </div>
+  );
+}
+
+export default App;
